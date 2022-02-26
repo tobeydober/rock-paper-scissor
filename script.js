40f4560b66b7215d8d6fd488e4ce9436b5bee839
Patrick <patrickhan37@gmail.com>
2022-02-25 21:11:37 -0500

    added event listeners and css

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ let count = 0
 let playerwins = 0
 let computerwins = 0
 let player_prompt = ''
+let p_choice = ''
+let c_choice = ''
 
-let computer_choice= function() {
+
+let computer_choice = function() {
     let choice = (Math.floor(Math.random()*3))
     if (choice < 1) {
         return 'rock'
@@ -15,32 +18,13 @@ let computer_choice= function() {
 }
 
 
-let player_choice = function() {
-    player_prompt = prompt("Pick rock, paper or scissor.")
-    ///check if answer
-    if (player_prompt == null) {
-        alert("try again!")
-        player_choice()
-    }
-    let p_choice = player_prompt.toLowerCase()
-    if (p_choice === "rock" || p_choice === "scissor" || p_choice === "paper") {
-        return p_choice
-    } else {
-        alert("Try again!")
-        player_choice()
-    }
-}
-
-
-
 /// keeps track of score 
-let playRound = function(x) {
+let playRound = (x) => {
     if (x) {
         playerwins += 1
     } else {
         computerwins += 1
     }
-    console.log((`The score is ${playerwins} to ${computerwins}`))
 }
 
 
@@ -75,23 +59,78 @@ let game = function(player, computer) {
     }
 }
 
+
+//alerts who wins the round
 let gameWinner = function(playerscore, computerscore) {
-    if (playerscore > computerwins) {
-        alert("You win!")
-    } else if (computerwins > playerscore) {
-            alert("You lose!")
+    if (playerscore > computerscore) {
+        alert("You win the round!")
+    } else if (computerscore > playerscore) {
+            alert("You lose the round!")
     } else {
         alert("Its a tie! Try again!")
     }
 }
 
-
-for (i = 0; i <= 5; i++) {
-    let c_choice = computer_choice()
-    let p_choice = player_choice()
-    game(p_choice,c_choice)
-    player_prompt = ''
+//checks if someone won up to 5 and resets score
+let endGame = function(playerScore, computerScore) {
+    if (playerScore === 5) {
+        alert('You win the game!')
+        playerwins = 0
+        computerwins = 0
+        score.textContent = (`The score is Player: ${playerwins} Computer: ${computerwins}`)
+    }
+    if (computerScore === 5) {
+        alert('You lose the game!')
+        playerwins = 0
+        computerwins = 0
+        score.textContent = (`The score is Player: ${playerwins} Computer: ${computerwins}`)
+    }
+    return
 }
 
 
-gameWinner(playerwins, computerwins)
+// making event listeners for player's choices
+
+
+//appends a div to scoreboard and changes the score
+let newScore = function() {
+    scoreboard.appendChild(score)
+    score.textContent = (`The score is Player: ${playerwins} Computer: ${computerwins}`)
+} 
+
+const paperClick = document.querySelector(".paperButton")
+const scissorClick = document.querySelector('.scissorButton')
+const rockClick = document.querySelector('.rockButton')
+const scoreboard = document.querySelector('.scoreboard')
+const score = document.createElement('div')
+const initialScore = document.querySelector('.initialScore')
+
+
+//these choices add an event listerner to buttons, picks a player
+//and computer choice, plays a game and change score
+paperClick.addEventListener('click', () => {
+    p_choice = 'paper'
+    c_choice = computer_choice()
+    game(p_choice, c_choice)
+    initialScore.remove()
+    newScore()
+    endGame(playerwins, computerwins)
+})
+
+scissorClick.addEventListener('click', () => {
+    p_choice ='scissor'
+    c_choice = computer_choice()
+    game(p_choice, c_choice)
+    initialScore.remove()
+    newScore()
+    endGame(playerwins, computerwins)
+})
+
+rockClick.addEventListener('click', () => {
+    p_choice = 'rock'
+    c_choice = computer_choice()
+    game(p_choice, c_choice)
+    initialScore.remove()
+    newScore()
+    endGame(playerwins, computerwins)
+})
